75a6473ecbebe8d1b950b80538b688645f73017b
Krzysztof Wycisło <krzysztofwycislo@gmail.com>
2020-12-08 14:19:04 +0100

    Added empty fragments before navigation component setup

---
+++ app/src/main/java/pl/handsome/club/repobrowser/ui/RepositoryDetailsFragment.kt
@@ -0,0 +1,6 @@
+package pl.handsome.club.repobrowser.ui
+
+import androidx.fragment.app.Fragment
+import pl.handsome.club.repobrowser.R
+
+class RepositoryDetailsFragment : Fragment(R.layout.fragment_repository_details)
---
+++ app/src/main/java/pl/handsome/club/repobrowser/ui/RepositoryListFragment.kt
@@ -0,0 +1,6 @@
+package pl.handsome.club.repobrowser.ui
+
+import androidx.fragment.app.Fragment
+import pl.handsome.club.repobrowser.R
+
+class RepositoryListFragment : Fragment(R.layout.fragment_repository_list)
