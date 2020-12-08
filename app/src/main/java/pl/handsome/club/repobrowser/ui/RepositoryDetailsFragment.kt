a3fd052a941221095f774cbdda977a8f6bab6e3b
Krzysztof Wycisło <krzysztofwycislo@gmail.com>
2020-12-08 14:52:55 +0100

    Added Navigation Component, setup single activity app structure

--- app/src/main/java/pl/handsome/club/repobrowser/ui/RepositoryDetailsFragment.kt
+++ app/src/main/java/pl/handsome/club/repobrowser/ui/RepositoryDetailsFragment.kt
@@ -3,4 +3,5 @@ package pl.handsome.club.repobrowser.ui
 import androidx.fragment.app.Fragment
 import pl.handsome.club.repobrowser.R
 
+
 class RepositoryDetailsFragment : Fragment(R.layout.fragment_repository_details)
--- app/src/main/java/pl/handsome/club/repobrowser/ui/RepositoryListFragment.kt
+++ app/src/main/java/pl/handsome/club/repobrowser/ui/RepositoryListFragment.kt
@@ -3,4 +3,5 @@ package pl.handsome.club.repobrowser.ui
 import androidx.fragment.app.Fragment
 import pl.handsome.club.repobrowser.R
 
+
 class RepositoryListFragment : Fragment(R.layout.fragment_repository_list)
